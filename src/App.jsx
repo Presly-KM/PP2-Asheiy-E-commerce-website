3e684c54cc2716c25f0014cb922896d03469392e
Presly K.MALONGO <preslykomba.m@gmail.com>
2025-05-30 13:34:36 +0200

    creation file Home + import + integration route home

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import UserLayout from "./components/Layout/UserLayout";
-
+import Home from "./pages/Home";
 const App = () => {
   return (
     <BrowserRouter>                                 {/* On utilise BrowserRouter pour gérer les routes. Les routes servent à naviguer entre différentes pages de l'application. Par exemple on pourrait avoir une route pour la page d'accueil, une autre pour la page de contact, etc. */}
-      
       <Routes>
          <Route path="/" element={<UserLayout/>}>   { /* La route "/" correspond à la page d'accueil de l'application. Le composant UserLayout sera affiché lorsque l'utilisateur visitera cette page. */}
-         {/* User Layout */}
+         <Route index element={<Home />} />         {/* La route index correspond à la page d'accueil de l'application. Elle est généralement utilisée pour afficher le contenu principal de l'application. */}
          </Route>
           <Route>{ /* Admin Layout */}</Route>
       </Routes>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Hero from '../components/Layout/Hero'
+import GenderCollectionSection from '../components/Products/GenderCollectionSection'
+import NewArrivals from '../components/Products/NewArrivals'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+        <Hero />
+        <GenderCollectionSection />
+        <NewArrivals />
+    </div>
   )
 }
 
