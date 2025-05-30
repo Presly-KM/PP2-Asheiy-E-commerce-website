e8202c065715ff8c8e480ecf9c81830a38d18af3
Presly K.MALONGO <preslykomba.m@gmail.com>
2025-05-30 09:58:09 +0200

    Fix grammar error

--- src/components/Layout/Topbar.jsx
+++ src/components/Layout/Topbar.jsx
@@ -18,7 +18,7 @@ const Topbar = () => {
           </a>
         </div>
         <div className=" text-sm text-center flex-grow">
-          <span>We ship worldwide - Faster and reliable shipping !</span>
+          <span>We ship worldwide - Fast and reliable shipping !</span>
       </div>
     <div className="text-sm hidden md:block">
         <a href="tel:+1234567890" className="hover:text-gray-300">
