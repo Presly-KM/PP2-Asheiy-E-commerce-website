3e64071bc12c84d6355bd96998820e451d2809f1
Presly K.MALONGO <preslykomba.m@gmail.com>
2025-05-30 13:36:47 +0200

    section gendercollection commit

--- src/components/Layout/UserLayout.jsx
+++ src/components/Layout/UserLayout.jsx
@@ -1,3 +1,4 @@
+import { Outlet } from "react-router-dom";
 import Footer from "../Common/Footer";
 import Header from "../Common/Header";
 
@@ -7,6 +8,10 @@ const UserLayout = () => {
       {/* Header */}
       <Header />
       {/* Main Content */}
+      <main>
+        <Outlet />                              {/* Outlet est utilisé pour afficher le contenu des routes imbriquées. Les routes imbriquées sont des routes qui sont définies à l'intérieur d'une autre route. Par exemple, si vous avez une route pour la page d'accueil et une autre pour la page de contact, vous pouvez utiliser Outlet pour afficher le contenu de ces pages à l'intérieur du composant UserLayout. Ici, Hero.jsx est un exemple de contenu qui sera affiché dans l'Outlet. Sans Oulet, Hero.jsx ne s'affiche pas*/}    
+      </main>
+
       {/* Footer */}
       <Footer />
     </>
