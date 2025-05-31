70a912ca4bde7e3b5cb848d7f96b0d16db749396
Presly K.MALONGO <preslykomba.m@gmail.com>
2025-05-31 13:00:15 +0200

    New arrivals + scrollables element 100% completed

--- src/components/Products/NewArrivals.jsx
+++ src/components/Products/NewArrivals.jsx
@@ -100,6 +100,23 @@ const NewArrivals = () => {
       ],
     },
   ];
+
+  const handleMouseDown = (e) => {                            // Ici on gère le début du glissement en utilisant l'événement onMouseDown. Lorsque l'utilisateur appuie sur le bouton de la souris, la fonction handleMouseDown est appelée. cf(explication 2:11:00)
+    setIsDragging(true);                                      // On met à jour l'état isDragging à true pour indiquer que l'utilisateur est en train de faire glisser le contenu.
+    setStartX(e.pageX - scrollRef.current.offsetLeft);        // On stocke la position X de départ du curseur en soustrayant la position gauche du conteneur de défilement. Cela permet de calculer la distance entre le curseur et le début du conteneur.
+    setScrollLeft(scrollRef.current.scrollLeft);              // On stocke la position de défilement initiale du conteneur avant le glissement. Cela permet de savoir où se trouve le contenu avant que l'utilisateur ne commence à faire glisser le conteneur.
+  };
+  
+  const handleMouseMove = (e) => {                           // Ici on gère le mouvement de la souris pendant le glissement en utilisant l'événement onMouseMove. Lorsque l'utilisateur déplace la souris tout en maintenant le bouton enfoncé, la fonction handleMouseMove est appelée.
+    if (!isDragging) return;                                 // On vérifie si l'utilisateur est en train de faire glisser le contenu. Si isDragging est false, on quitte la fonction pour ne pas effectuer d'actions inutiles.
+    const x = e.pageX - scrollRef.current.offsetLeft;        // On calcule la position X actuelle du curseur en soustrayant la position gauche du conteneur de défilement. Cela permet de savoir où se trouve le curseur par rapport au conteneur.
+    const walk = (x - startX);                               // On calcule la distance de glissement en multipliant la différence entre la position actuelle du curseur et la position de départ par 2. Cela permet d'accélérer le glissement pour une meilleure expérience utilisateur.
+    scrollRef.current.scrollLeft = scrollLeft - walk;        // On met à jour la position de défilement du conteneur en soustrayant la distance de glissement de la position de défilement initiale. Cela permet de faire défiler le contenu horizontalement en fonction du mouvement de la souris.
+  };                            
+ 
+  const handleMouseUpOrLeave = () => {                       // Ici on gère la fin du glissement en utilisant l'événement onMouseUp. Lorsque l'utilisateur relâche le bouton de la souris, la fonction handleMouseUp est appelée. 
+    setIsDragging(false);                                    // On met à jour l'état isDragging à false pour indiquer que l'utilisateur a terminé de faire glisser le contenu.
+  };                     
   
   const scroll = (direction) => {                                           // Ici on gère le défilement du contenu horizontalement en fonction de la direction (gauche ou droite) passée en paramètre. Cette fonction scroll est appelée lorsque l'utilisateur clique sur les boutons (gauche ou droite) de défilement.
     const scrollAmount = direction === "left" ? -300 : 300;                 // On définit la quantité de défilement en pixels. Si la direction est "left", on défile vers la gauche (-300 pixels), sinon on défile vers la droite (300 pixels). 
@@ -118,10 +135,12 @@ const NewArrivals = () => {
       setCanScrollRight(rightScrollable);                            // On peut scroller vers la droite si le contenu est plus large que ce qui est visible. Cela signifie qu'il y a du contenu supplémentaire à droite qui peut être défilé. // Il s'agissait ici de verifier si l'utilisateur a atteint la fin du contenu défilable. le contenu total à défiler (y compris ce qui n'est pas visible) est de 3854 pixels. Ainsi on additione la position actuelle du défilement (leftScroll "sa valeur évolue avec le défilement enclenché par l'utilisateur") à la largeur visible du conteneur (clientWidth "dont la valeur est constamment égale à 1536"). Si la somme des deux est inférieure à la largeur totale du contenu (scrollWidth "dont la valeur est constamment égale à 3854"), cela signifie qu'il y a encore du contenu à droite qui peut être défilé. Dans ce cas setCanScrollLeft s'applique et agit pour mettre à jour l'état canScrollRight à "true", ce qui active l'icône de défilement vers la droite. Sinon (Si la somme est inférieure à la largeur totale du contenu "3854") alors on met à jour l'état canScrollRight à "false", ce qui désactive l'icône de défilement vers la droite.
     }
 
+   
     console.log({
       scrollLeft: container.scrollLeft,
       clientWidth: container.clientWidth,
       containerScrollWidth: container.scrollWidth,
+      offsetLeft : scrollRef.current.offsetLeft,
     });
   };
                                                                  // UseEffect sert à gérer les effets de bord dans les composants fonctionnels. Il est exécuté après le rendu du composant et peut être utilisé pour effectuer des opérations telles que la récupération de données, l'ajout d'écouteurs d'événements, ou la mise à jour du DOM.
@@ -130,11 +149,12 @@ const NewArrivals = () => {
     if (container) {                                             // Ici la référence (actuelle ?) scrollRef.current est vérifiée pour s'assurer qu'elle est définie avant d'ajouter l'écouteur d'événement de défilement. Cela évite les erreurs si la référence n'est pas encore attachée à un élément DOM.
       container.addEventListener("scroll", updateScrollButtons); // On ajoute un écouteur d'événement de défilement au conteneur pour mettre à jour les boutons de défilement.
       updateScrollButtons();                                     // On appelle la fonction pour mettre à jour les boutons de défilement immédiatement après l'ajout de l'écouteur d'événement.                   
+      return () => container.removeEventListener("scroll", updateScrollButtons); // On retourne une fonction de nettoyage qui supprime l'écouteur d'événement de défilement lorsque le composant est démonté ou que la référence change. Cela permet d'éviter les fuites de mémoire et les erreurs liées à des écouteurs d'événements obsolètes.                                       
     }
-  });
-
+  }, []);
+  
   return (
-    <section>
+    <section className="py-16 px-4 lg:px-0">
       <div className="container mx-auto text-center mb-10 relative">
         <h2 className="text-3xl font-bold mb-4">Explore New Arrivals</h2>
         <p className="text-lg text-gray-600 mb-8">
@@ -171,8 +191,13 @@ const NewArrivals = () => {
       {/* Scrollable Content */}
       <div
         ref={scrollRef}                      // Ici on attache la référence au défileur du conteneur . Car plus haut on a créé l'option "scrollRef" pour cheker la référence et accéder aux propriétés de défilement du conteneur.
-        className="container mx-auto overflow-x-scroll flex space-x-6 relative"
-      >
+        className={`container mx-auto overflow-x-scroll flex space-x-6 relative ${
+          isDragging ? "cursor-grabbing" : "cursor-grab"}`} 
+        onMouseDown={handleMouseDown}            // Ici on gère le début du glissement en utilisant l'événement onMouseDown. Lorsque l'utilisateur appuie sur le bouton de la souris, la fonction handleMouseDown est appelée.
+        onMouseMove={handleMouseMove}          // Ici on gère le mouvement de la souris pendant le glissement en utilisant l'événement onMouseMove. Lorsque l'utilisateur déplace la souris tout en maintenant le bouton enfoncé, la fonction handleMouseMove est appelée.
+        onMouseUp={handleMouseUpOrLeave}              // Ici on gère la fin du glissement en utilisant l'événement onMouseUp. Lorsque l'utilisateur relâche le bouton de la souris, la fonction handleMouseUp est appelée.
+        onMouseLeave={handleMouseUpOrLeave}        // Ici on gère le cas où la souris quitte le conteneur pendant le glissement en utilisant l'événement onMouseLeave. Si l'utilisateur quitte le conteneur tout en maintenant le bouton de la souris enfoncé, la fonction handleMouseLeave est appelée.
+     >
         {newArrivals.map((product) => (
           <div
             key={product._id}
@@ -182,6 +207,7 @@ const NewArrivals = () => {
               src={product.images[0]?.url}
               alt={product.images[0]?.altText || product.name}
               className="w-full h-[500px] object-cover rounded-lg"
+              draggable="false"                    // Pour empêcher le glissement de l'image lors du défilement horizontal.
             />
             <div
               className="absolute bottom-0 left-0 right-0 bg-opacity-50 backdrop-blur-md text-white 
