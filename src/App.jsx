99a8f11c4b96110825a3b6765da14a365bbe9b71
Presly K.MALONGO <preslykomba.m@gmail.com>
2025-06-01 14:10:09 +0200

    toaster notification library

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import UserLayout from "./components/Layout/UserLayout";
 import Home from "./pages/Home";
+import { Toaster } from "sonner";
+
 const App = () => {
   return (
     <BrowserRouter>                                 {/* On utilise BrowserRouter pour gérer les routes. Les routes servent à naviguer entre différentes pages de l'application. Par exemple on pourrait avoir une route pour la page d'accueil, une autre pour la page de contact, etc. */}
+      <Toaster position="top-right"/>               {/* Toaster est utilisé pour afficher des notifications à l'utilisateur. Par exemple, lorsqu'une action réussit ou échoue, une notification peut être affichée pour informer l'utilisateur. */}
       <Routes>
          <Route path="/" element={<UserLayout/>}>   { /* La route "/" correspond à la page d'accueil de l'application. Le composant UserLayout sera affiché lorsque l'utilisateur visitera cette page. */}
          <Route index element={<Home />} />         {/* La route index correspond à la page d'accueil de l'application. Elle est généralement utilisée pour afficher le contenu principal de l'application. */}
@@ -13,4 +16,5 @@ const App = () => {
     </BrowserRouter>
   );
 };
+
 export default App;
