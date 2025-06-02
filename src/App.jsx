4a4398f47d8d548fc3038f1d595c29465bd480aa
Presly K.MALONGO <preslykomba.m@gmail.com>
2025-06-02 13:23:49 +0200

    add register profile and collection

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import UserLayout from "./components/Layout/UserLayout";
 import Home from "./pages/Home";
 import { Toaster } from "sonner";
 import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Profile from "./pages/Profile";
 
 const App = () => {
   return (
@@ -12,6 +14,8 @@ const App = () => {
          <Route path="/" element={<UserLayout/>}>   { /* La route "/" correspond à la page d'accueil de l'application. Le composant UserLayout sera affiché lorsque l'utilisateur visitera cette page. */}
          <Route index element={<Home />} />         {/* La route index correspond à la page d'accueil de l'application. Elle est généralement utilisée pour afficher le contenu principal de l'application. */}
          <Route path="login" element={<Login />} /> {/* La route "login" correspond à la page de connexion de l'application. Lorsque l'utilisateur visitera cette page, le composant Login sera affiché. */}
+          <Route path="register" element={<Register />} /> {/* La route "register" correspond à la page d'inscription de l'application. Le composant Register est utilisé ici, mais il pourrait être remplacé par un composant d'inscription spécifique. */}
+          <Route path="profile" element={<Profile />} /> {/* La route "profile" correspond à la page de profil de l'utilisateur. Le composant Profile sera affiché lorsque l'utilisateur visitera cette page. */}
          </Route>
           <Route>{ /* Admin Layout */}</Route>
       </Routes>
