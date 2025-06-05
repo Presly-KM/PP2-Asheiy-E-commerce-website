1f64797e3a913cf027b61f75276528b38eeccdbc
Presly K.MALONGO <preslykomba.m@gmail.com>
2025-06-05 17:04:49 +0200

    {calculatedEstimatedDelivery(checkout.createdAt)}

--- src/pages/OrderConfirmationPage.jsx
+++ src/pages/OrderConfirmationPage.jsx
@@ -1,58 +1,64 @@
-import React from 'react'
+import React from "react";
 const checkout = {
-    _id: "12345",
-    createdAt: new Date(),
-    checkoutItems: [
-        {
-            productId: "1",
-            name: "Jacket",
-            color:"black",
-            size: "M",
-            price: 150,
-            quantity: 1,
-            image: "https://picsum.photos/150?random=1",
-        },
-        {
-            productId: "2",
-            name: "T-shirt",
-            color:"black",
-            size: "M",
-            price: 120,
-            quantity: 2,
-            image: "https://picsum.photos/150?random=2",
-        },
-    ],
-    shippingAddress: {
-        address: "123 Fashion Street",
-        city: "New York",
-        country: "USA",
+  _id: "12345",
+  createdAt: new Date(),
+  checkoutItems: [
+    {
+      productId: "1",
+      name: "Jacket",
+      color: "black",
+      size: "M",
+      price: 150,
+      quantity: 1,
+      image: "https://picsum.photos/150?random=1",
     },
-}
+    {
+      productId: "2",
+      name: "T-shirt",
+      color: "black",
+      size: "M",
+      price: 120,
+      quantity: 2,
+      image: "https://picsum.photos/150?random=2",
+    },
+  ],
+  shippingAddress: {
+    address: "123 Fashion Street",
+    city: "New York",
+    country: "USA",
+  },
+};
 
 const OrderConfirmationPage = () => {
   return (
-    <div className='max-w-4xl mx-auto p-6 bg-white'>
-        <h1 className='text-4xl font-bold text-center text-emerald-700 mb-8'>
-            Thank You for Your Order!
-        </h1>
+    <div className="max-w-4xl mx-auto p-6 bg-white">
+      <h1 className="text-4xl font-bold text-center text-emerald-700 mb-8">
+        Thank You for Your Order!
+      </h1>
 
-        {checkout && ( 
-            <div className="p-6 rounded-lg border">
-                <div className="flex justify-between mb-20">
-                  {/* Order Id and Date */}
-                <div>
-                    <h2 className="text-xl font-semibold ">
-                        Order ID: {checkout._id}
-                    </h2>
-                    <p className="text-gray-500">
-                        Order date: {new Date(checkout.createdAt).toLocaleDateString()}
-                    </p>
-                </div>
-                </div>
-
+      {checkout && (
+        <div className="p-6 rounded-lg border">
+          <div className="flex justify-between mb-20">
+            {/* Order Id and Date */}
+            <div>
+              <h2 className="text-xl font-semibold ">
+                Order ID: {checkout._id}
+              </h2>
+              <p className="text-gray-500">
+                Order date: {new Date(checkout.createdAt).toLocaleDateString()}
+              </p>
+            </div>
+            {/* Estimated Delivery */}
+            <div>
+              <p className="text-emerald-700 text-sm">
+                Estimated Delivery: {""}
+                {calculatedEstimatedDelivery(checkout.createdAt)}
+              </p>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
-  )}
-</div>
   );
-}
-export default OrderConfirmationPage
+};
+export default OrderConfirmationPage;
