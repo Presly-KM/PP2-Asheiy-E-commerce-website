fbf097b92e40446855fe103ec199bc6caa6568be
Presly K.MALONGO <preslykomba.m@gmail.com>
2025-06-05 10:43:23 +0200

    Add file

--- src/components/Cart/PayPalButton.jsx
+++ src/components/Cart/PayPalButton.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import { PayPalButtons, PayPalScriptProvider } from "@paypal/react-paypal-js";
 
-const PayPalButton = () => {
+
+const PayPalButton = ({amount, onSuccess, onError}) => {
   return (
-    <div>PayPalButton</div>
-  )
-}
+    <PayPalScriptProvider
+      options={{
+        "client-id":
+          "AVrssN7XQ3aediwJECJu0kZCR83mv3lsNu9WItOnwaPXmIJ--18uW1aRtHjGwyWGRBTycO7MUxS14L3t",
+      }}
+    >
+      <PayPalButtons
+        style={{ layout: "vertical" }}
+        createOrder={(data, actions) => {
+          return actions.order.create({
+            purchase_units: [{ amount: { value: amount } }], // Replace with the total price of the cart
+          });
+        }}
+        onApprove={(data, actions) => {
+          return actions.order.capture().then(onSuccess);
+        }}
+        onError={onError}
+      />
+    </PayPalScriptProvider>
+  );
+};
 
-export default PayPalButton
+export default PayPalButton;
