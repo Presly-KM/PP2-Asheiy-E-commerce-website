471d9356d9020afec5013f31bb600c2a3899c70b
Presly K.MALONGO <preslykomba.m@gmail.com>
2025-06-06 11:24:16 +0200

    useNavigate

--- src/pages/MyOrdersPage.jsx
+++ src/pages/MyOrdersPage.jsx
@@ -1,34 +1,37 @@
-import React, { useEffect } from 'react'
+import { useEffect } from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom"; // Importing useNavigate from react-router-dom to handle navigation
 
-const MyOrdersPage= () => {
-  const [orders, setOrders] = React.useState([])
+const MyOrdersPage = () => {
+  const [orders, setOrders] = useState([]);
+  const navigate = useNavigate(); // Importing useNavigate from react-router-dom to handle navigation
 
   useEffect(() => {
     //Simulate fetching orders
     setTimeout(() => {
       const mockOrders = [
-        { 
-          _id: 12345, 
-          createdAt: new Date(), 
-          shippingAdress: {city: "New York", country: "USA"},
+        {
+          _id: 12345,
+          createdAt: new Date(),
+          shippingAdress: { city: "New York", country: "USA" },
           orderItems: [
-            { 
+            {
               name: "Product 1",
               image: "https://picsum.photos/500/500?random=1",
-             },
+            },
           ],
           totalPrice: 100,
           isPaid: true,
         },
-        { 
-          _id: 67890, 
-          createdAt: new Date(), 
-          shippingAdress: {city: "Los Angeles", country: "USA"},
+        {
+          _id: 67890,
+          createdAt: new Date(),
+          shippingAdress: { city: "Los Angeles", country: "USA" },
           orderItems: [
-            { 
+            {
               name: "Product 2",
               image: "https://picsum.photos/500/500?random=2",
-             },
+            },
           ],
           totalPrice: 100,
           isPaid: true,
@@ -36,11 +39,14 @@ const MyOrdersPage= () => {
       ];
       setOrders(mockOrders);
     }, 1000);
-  }, []);                   // [] Veut dire que l'effet ne s'exécute qu'une seule fois, au montage du composant. Cela est utile pour simuler un appel API ou une action qui ne doit se produire qu'une seule fois, comme le chargement initial des données. C'est à dire que l'effet ne sera pas réexécuté lors des mises à jour du composant, ce qui est souvent le cas pour les appels API ou les initialisations de données.
+  }, []); // [] Veut dire que l'effet ne s'exécute qu'une seule fois, au montage du composant. Cela est utile pour simuler un appel API ou une action qui ne doit se produire qu'une seule fois, comme le chargement initial des données. C'est à dire que l'effet ne sera pas réexécuté lors des mises à jour du composant, ce qui est souvent le cas pour les appels API ou les initialisations de données.
 
+  const handleRowClick = (orderId) => {
+    navigate(`/order/${orderId}`); // Navigate to the order details page with the clicked order ID
+  };
   return (
-    <div className='max-w-7xl mx-auto p-4 sm:p-6'>
-      <h2 className='text-xl sm:text-2xl font-bold mb-6'> My Orders </h2>
+    <div className="max-w-7xl mx-auto p-4 sm:p-6">
+      <h2 className="text-xl sm:text-2xl font-bold mb-6"> My Orders </h2>
       <div className="relative shadow-md sm:rounded-lg overflow-hidden">
         <table className="min-w-full text-left text-gray-500">
           <thead className="bg-gray-100 text-xs uppercase text-gray-700">
@@ -57,8 +63,10 @@ const MyOrdersPage= () => {
           <tbody>
             {orders.length > 0 ? (
               orders.map((order) => (
-                <tr key={order._id} 
-                    className="border-b hover:border-gray-50 cursor-pointer "
+                <tr
+                  key={order._id}
+                  onClick={() => handleRowClick(order._id)} // Assuming handleRowClick is a function to navigate to order details
+                  className="border-b hover:border-gray-50 cursor-pointer "
                 >
                   <td className="py-2 px-2 sm:py-4 sm:px-4">
                     <img
@@ -67,41 +75,45 @@ const MyOrdersPage= () => {
                       className="w-10 h-10 sm:w-12 sm:h-12 object-cover rounded-lg"
                     />
                   </td>
-                   <td className="py-2 px-2 sm:py-4 sm:px-4 font-medium text-gray-900 
-                   whitespace-nowrap">#{order._id}
-                   </td>
-                   <td className="py-2 px-2 sm:py-4 sm:px-4">
-                    {new Date(order.createdAt).toLocaleDateString()}{""}
+                  <td
+                    className="py-2 px-2 sm:py-4 sm:px-4 font-medium text-gray-900 
+                   whitespace-nowrap"
+                  >
+                    #{order._id}
+                  </td>
+                  <td className="py-2 px-2 sm:py-4 sm:px-4">
+                    {new Date(order.createdAt).toLocaleDateString()}
+                    {""}
                     {new Date(order.createdAt).toLocaleTimeString()}
                   </td>
                   <td className="py-2 px-2 sm:py-4 sm:px-4">
-                    {order.shippingAdress 
-                    ? ` ${order.shippingAdress.city}, ${order.shippingAdress.country}` 
-                    : "N/A"}
+                    {order.shippingAdress
+                      ? ` ${order.shippingAdress.city}, ${order.shippingAdress.country}`
+                      : "N/A"}
                   </td>
                   <td className="py-2 px-2 sm:py-4 sm:px-4">
                     {order.orderItems.length}
-                    </td>
+                  </td>
                   <td className="py-2 px-2 sm:py-4 sm:px-4">
                     ${order.totalPrice}
                   </td>
                   <td className="py-2 px-2 sm:py-4 sm:px-4">
-                    <span 
-                    className={`${
-                      order.isPaid 
-                    ? "bg-green-100 text-green-700" 
-                    : "bg-red-100 text-red-700"
-                    } px-2 py-1 rounded-full text-xs sm:text-sm font-medium `}
+                    <span
+                      className={`${
+                        order.isPaid
+                          ? "bg-green-100 text-green-700"
+                          : "bg-red-100 text-red-700"
+                      } px-2 py-1 rounded-full text-xs sm:text-sm font-medium `}
                     >
                       {order.isPaid ? "Paid" : "Pending"}
                     </span>
                   </td>
-                 </tr>
+                </tr>
               ))
             ) : (
               <tr>
                 <td colSpan="7" className="py-4 px-4 text-center text-gray-500">
-                  You have no orders 
+                  You have no orders
                 </td>
               </tr>
             )}
@@ -109,6 +121,6 @@ const MyOrdersPage= () => {
         </table>
       </div>
     </div>
-  )
-}
+  );
+};
 export default MyOrdersPage;
