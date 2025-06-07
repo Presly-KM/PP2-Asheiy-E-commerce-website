51457584ddd8687749a5c9bb50039e76632f8447
Presly K.MALONGO <preslykomba.m@gmail.com>
2025-06-07 12:21:43 +0200

    correct picsum url adress

--- src/components/Admin/EditProductPage.jsx
+++ src/components/Admin/EditProductPage.jsx
@@ -16,10 +16,10 @@ const EditProductPage = () => {
     gender: "",
     images: [
       {
-        url: "https://picsum.photos.150?random=1",
+        url: "https://picsum.photos/150?random=1",
       },
       {
-        url: "https://picsum.photos.150?random=2",
+        url: "https://picsum.photos/150?random=2",
       },
     ],
   });
