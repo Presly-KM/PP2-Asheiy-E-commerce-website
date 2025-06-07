5f06a2a6491ae2bb4bfb326af1169ff92c087a63
Presly K.MALONGO <preslykomba.m@gmail.com>
2025-06-07 09:34:45 +0200

    Add New user form

---
+++ src/components/Admin/UserManagement.jsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+
+const UserManagement = () => {
+  const users = [
+    {
+      name: "John Doe",
+      email: " john@example.com",
+      role: "admin",
+    },
+  ];
+
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    role: "customer", // Default role
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+        [e.target.name]: e.target.value,     // Ce e.target.name va avoir pour role de récupérer le nom de l'input qui a été modifié, et e.target.value va récupérer la valeur actuelle de cet input.
+    });
+  };
+
+  return (
+    <div className="max-w-7xl mx-auto p-6">
+      <h2 className="text-2xl font-bold mb-6">User Management</h2>
+      {/* Add New User Form */}
+      <div className="p-6 rounded-lg mb-6">
+        <h3 className="text-xl font-bold mb-4">Add New User</h3>
+        <form>
+          <div className="mb-4">
+            <label className="block text-gray-700"> Name</label>
+            <input
+              type="text"
+              name="name"
+                value={formData.name}
+                onChange={handleChange}
+                className="w-full p-2 border rounded"
+                required
+            />
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+export default UserManagement;
